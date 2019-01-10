cbf595e4068b728a97f1ca72f9d10aebc04924db
Jovan Romic <jovan.romic@elfak.rs>
2019-01-10 18:31:56 +0100

    Delete customers al ne brise

--- NBPDom1i2/NBPDom1i2/Controllers/CustomersController.cs
+++ NBPDom1i2/NBPDom1i2/Controllers/CustomersController.cs
@@ -1,4 +1,5 @@
 ﻿using NBPDom1i2.Models;
+using NBPDom1i2.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +13,27 @@ namespace NBPDom1i2.Controllers
         // GET: Customers
         public ActionResult Index()
         {
-            return View();
+            //Customers customers = new Customers();
+
+            var data = WebApiConfig.GraphClient.Cypher
+                .Match("(c:Customer)")
+                .Return(c => c.As<Customer>())
+                .Results.ToList();
+
+            var model = new CustomersSelectionViewModel();
+
+            foreach (var customer in data)
+            {
+                var editorViewModel = new SelectCustomerEditorViewModel()
+                {
+                    name = string.Format("{0} {1}", customer.name, customer.surname),
+                    username = customer.username,
+                    selected = false
+                };
+                model.customers.Add(editorViewModel);
+            }
+
+            return View(model);
         }
 
         public ActionResult NewCustomer()
@@ -27,10 +48,29 @@ namespace NBPDom1i2.Controllers
             Dictionary<string, object> dictionary = new Dictionary<string, object>();
             dictionary.Add("name", customer.name);
             dictionary.Add("surname", customer.surname);
-            WebApiConfig.GraphClient.Cypher.Create("(customer:Customer {name: {name}, surname: {surname}})")
+            dictionary.Add("username", customer.username);
+            WebApiConfig.GraphClient.Cypher.Create("(customer:Customer {name: {name}, surname: {surname}, username: {username}})")
                 .WithParams(dictionary).ExecuteWithoutResults();
 
             return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public ActionResult DeleteCustomer(CustomersSelectionViewModel customers)
+        {
+            /*Dictionary<string, object> dictionary = new Dictionary<string, object>();
+            dictionary*/
+            var selectedUsernames = customers.getSelectedUsernames(); //ovde imam sve usernamee za brisanje
+            foreach (var su in selectedUsernames)
+            {
+                Dictionary<string, object> dictionary = new Dictionary<string, object>();
+                dictionary.Add("username", su);
+                WebApiConfig.GraphClient.Cypher.Delete("(customer:Customer {username: {username}})")
+                .WithParams(dictionary).ExecuteWithoutResults();
+
+                //return RedirectToAction("Index", "Customers");
+            }
+            return RedirectToAction("Index", "Customers");
         }
     }
 }
---
+++ NBPDom1i2/NBPDom1i2/Models/SelectCustomerEditorViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NBPDom1i2.Models;
+
+namespace NBPDom1i2.Models
+{
+    public class SelectCustomerEditorViewModel
+    {
+        public bool selected { get; set; }
+        public string name { get; set; }
+        //public string surname { get; set; }
+        public string username { get; set; }
+        //public Customer customer { get; set; }
+    }
+}
